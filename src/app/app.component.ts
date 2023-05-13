1979a64a96f4c5f0f4710a46b0c6619195c575c8
carlos benavides <1992cab@gmail.com>
2023-05-13 14:47:35 -0500

    feature: added check string array method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'dna';
+  inputValue = '';
+  result = '';
+
+  handleInput(evt: Event) {
+    this.inputValue = (evt.target as HTMLInputElement).value;
+  }
+
+  handleSubmit(evt: Event) {
+    evt.preventDefault();
+    this.isMutant(this.inputValue.split(','));
+  }
+
+  isMutant(dna: string[]) {
+    this.result = '';
+
+    for (const element of dna) {
+      let currentLetter = element[0];
+      let count = 1;
+
+      for (let j = 1; j < element.length; j++) {
+        if (element[j] === currentLetter) {
+          count++;
+        } else {
+          currentLetter = element[j];
+          count = 1;
+        }
+        if (count === 4) {
+          this.result = 'MUTANTE';
+          break;
+        }
+      }
+    }
+
+    if (!this.result) {
+      this.result = 'HUMANO';
+    }
+  }
 }
